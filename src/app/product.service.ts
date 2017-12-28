31509c0345fff61d414ae0c8f9192548e102e24e
Luis <luisdelae@gmail.com>
2017-12-28 12:15:38 -0600

    Fixing json response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   private _albumUrl: string = '../assets/album.json';
 
   getAlbum(id:number) {
-    return this._http.get(this._albumUrl).map((response) => response.json);
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
   constructor(private _http: Http) {  }
